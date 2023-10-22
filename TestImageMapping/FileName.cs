5ebe489c7bcb32329420a423ed3716fb35cc6283
Christopher Hood <majianlord@gmail.com>
2023-10-22 15:01:51 -0700

    Add project files.

---
+++ TestImageMapping/FileName.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestImageMapping
+{
+    internal class FileName
+    {
+
+
+
+
+        //static void Main2()
+        //{
+        //    // Load the sample image
+        //    Bitmap sampleImage = new Bitmap("sample.tif");
+
+        //    // Load the template image
+        //    Bitmap templateImage = new Bitmap("template.tif");
+
+        //    // Perform template matching
+        //    Size regionSize = new Size(templateImage.Width, templateImage.Height);
+
+        //    // Define the overlap between regions (this can be adjusted as needed)
+        //    int overlap = regionSize.Height / 10;
+
+        //    // Divide the sample image into overlapping regions
+        //    List<Bitmap> regions = DivideIntoOverlappingRegions2(sampleImage, regionSize, overlap);
+
+        //    // Perform template matching on each region and combine results
+        //    List<Point> globalMatches = new List<Point>();
+        //    foreach (var region in regions)
+        //    {
+        //        List<Point> localMatches = PerformTemplateMatchingOnRegion(region, templateImage);
+        //        // Assuming the regions are in row-major order
+        //        int regionX = (region.Width - overlap) * (globalMatches.Count % regions.Count);
+        //        int regionY = (region.Height - overlap) * (globalMatches.Count / regions.Count);
+        //        List<Point> adjustedMatches = CombineResults(localMatches, regionX, regionY);
+        //        globalMatches.AddRange(adjustedMatches);
+        //    }
+
+        //    List<Bitmap> matchedTemplates = ExtractMatchedTemplates(sampleImage, templateImage, globalMatches);
+        //    // Save the matched templates
+        //    for (int i = 0; i < matchedTemplates.Count; i++)
+        //    {
+        //        matchedTemplates[i].Save($"MatchedTemplate{i + 1}.jpg");
+        //        matchedTemplates[i].Dispose(); // Dispose of the bitmap after saving it
+        //    }
+
+        //    Console.WriteLine("Processing complete!");
+        //}
+
+        //static List<Bitmap> ExtractMatchedTemplates(Bitmap originalImage, Bitmap templateImage, List<Point> globalMatches)
+        //{
+        //    List<Bitmap> matchedTemplates = new List<Bitmap>();
+
+        //    foreach (Point location in globalMatches)
+        //    {
+        //        // Define the region to crop based on the template size and the match location
+        //        Rectangle cropArea = new Rectangle(location, templateImage.Size);
+
+        //        // Crop the region from the original image and add it to the list
+        //        Bitmap matchedTemplate = new Crop(cropArea).Apply(originalImage);
+        //        matchedTemplates.Add(matchedTemplate);
+        //    }
+
+        //    return matchedTemplates;
+        //}
+
+
+        //static List<Point> CombineResults(List<Point> localMatches, int regionX, int regionY)
+        //{
+        //    List<Point> globalMatches = new List<Point>();
+        //    foreach (var match in localMatches)
+        //    {
+        //        globalMatches.Add(new Point(match.X + regionX, match.Y + regionY));
+        //    }
+        //    return globalMatches;
+        //}
+
+
+        //static List<Bitmap> DivideIntoOverlappingRegions2(Bitmap image, Size regionSize, int overlap)
+        //{
+        //    if (regionSize.Height <= 0)
+        //        throw new ArgumentException("Region height must be positive");
+
+        //    if (regionSize.Height > image.Height)
+        //        throw new ArgumentException("Region height must be smaller than or equal to the image height");
+
+        //    List<Bitmap> regions = new List<Bitmap>();
+
+        //    for (int y = 0; y <= image.Height - regionSize.Height; y += regionSize.Height - overlap)
+        //    {
+        //        // Always use the full width of the image
+        //        int regionWidth = image.Width;
+
+        //        Rectangle regionRect = new Rectangle(0, y, regionWidth, regionSize.Height);
+        //        Bitmap region = new Bitmap(width:regionRect.Width, height:regionRect.Height);
+        //        //Bitmap region = new Bitmap(width: 4734, height: 925);
+
+        //        using (Graphics g = Graphics.FromImage(region))
+        //        {
+        //            g.DrawImage(image, 0, 0, regionRect, GraphicsUnit.Pixel);
+        //        }
+
+
+        //        regions.Add(EnsureGrayscale(region));
+        //    }
+
+        //    return regions;
+        //}
+
+
+
+
+        //static Bitmap EnsureGrayscale(Bitmap image)
+        //{
+        //    if (image.PixelFormat != PixelFormat.Format8bppIndexed)
+        //    {
+        //        // Convert the temporary image to grayscale using AForge.NET
+        //        Grayscale grayscaleFilter = Grayscale.CommonAlgorithms.BT709;
+        //        Bitmap grayscaleImage = grayscaleFilter.Apply(image);
+        //        return grayscaleImage;
+        //    }
+        //    return image;
+        //}
+
+        //static List<Point> PerformTemplateMatchingOnRegion(Bitmap region, Bitmap template)
+        //{
+        //    // Ensure the images are in grayscale
+        //    Bitmap regionGray = EnsureGrayscale(region);
+        //    Bitmap templateGray = EnsureGrayscale(template);
+
+        //    Bitmap sampleGrayPad = PadToPowerOfTwo(regionGray);
+
+        //    // Apply the Fast Matched Filter
+        //    Bitmap correlationMap = FastMatchedFilter(sampleGrayPad, templateGray);
+
+        //    // Locate peaks in the correlation map
+        //    List<Point> peakLocations = FindPeaks(correlationMap);
+
+        //    // Since we are working with a region, we don't need to extract the matched templates here
+        //    // We just return the peak locations
+        //    return peakLocations;
+        //}
+
+
+
+        //static List<Bitmap> PerformTemplateMatching(Bitmap sample, Bitmap template)
+        //{
+
+        //    Bitmap sampleGray = EnsureGrayscale(sample);
+        //    Bitmap templateGray = EnsureGrayscale(template);
+
+        //    Bitmap sampleGrayPad = PadToPowerOfTwo(sampleGray);
+        //    sampleGray.Dispose();
+
+        //    // Apply the Fast Matched Filter
+        //    Bitmap correlationMap = FastMatchedFilter(sampleGrayPad, templateGray);
+        //    sampleGrayPad.Dispose();
+
+        //    // Locate peaks in the correlation map
+        //    List<Point> peakLocations = FindPeaks(correlationMap);
+
+        //    // Extract matched templates from the original image
+        //    List<Bitmap> matchedTemplates = new List<Bitmap>();
+        //    foreach (Point location in peakLocations)
+        //    {
+        //        Bitmap matchedTemplate = new Accord.Imaging.Filters.Crop(new Rectangle(location, template.Size)).Apply(sample);
+        //        matchedTemplates.Add(matchedTemplate);
+        //    }
+
+        //    return matchedTemplates;
+        //}
+        //static Bitmap FastMatchedFilter(Bitmap sample, Bitmap template)
+        //{
+        //    // Pad the template image to match the size of the sample image
+        //    Bitmap paddedTemplate = PadToPowerOfTwoV2(template, sample.Width, sample.Height);
+
+        //    // Convert bitmaps to complex image format
+        //    ComplexImage sampleComplex = ComplexImage.FromBitmap(sample);
+        //    ComplexImage templateComplex = ComplexImage.FromBitmap(paddedTemplate);
+
+        //    // Apply Fourier Transform
+        //    sampleComplex.ForwardFourierTransform();
+        //    templateComplex.ForwardFourierTransform();
+
+        //    // Check dimensions
+        //    if (sampleComplex.Width != templateComplex.Width || sampleComplex.Height != templateComplex.Height)
+        //    {
+        //        throw new InvalidOperationException("Dimensions of sample and template complex images do not match.");
+        //    }
+        //    var Width = templateComplex.Data.GetLength(1);
+        //        var Height = templateComplex.Data.GetLength(0);
+
+        //    if (Width != templateComplex.Width || Height != templateComplex.Height)
+        //    {
+        //        throw new InvalidOperationException("Dimensions of the templateComplex data array do not match its width and height.");
+        //    }
+
+        //    // Conjugate the template image in the frequency domain
+        //    for (int y = 0; y < templateComplex.Width; y++)
+        //    {
+        //        for (int x = 0; x < templateComplex.Height; x++)
+        //        {
+        //            templateComplex.Data[x, y] = Complex.Conjugate(templateComplex.Data[x, y]);
+        //        }
+        //    }
+
+        //    // Perform element-wise multiplication in the frequency domain
+        //    ComplexImage resultComplex = ComplexImage.FromBitmap(new Bitmap(sampleComplex.Width, sampleComplex.Height,PixelFormat.Format8bppIndexed));
+        //    for (int y = 0; y < sampleComplex.Width; y++)
+        //    {
+        //        for (int x = 0; x < sampleComplex.Height; x++)
+        //        {
+        //            resultComplex.Data[x, y] = sampleComplex.Data[x, y] * templateComplex.Data[x, y];
+        //        }
+        //    }
+
+        //    // Apply Inverse Fourier Transform
+        //    resultComplex.BackwardFourierTransform();
+
+        //    // Convert back to bitmap
+        //    Bitmap resultBitmap = resultComplex.ToBitmap();
+
+        //    return resultBitmap;
+        //}
+
+
+
+
+
+
+
+
+        //static Bitmap PadToPowerOfTwo(Bitmap image)
+        //{
+        //    int width = NextPowerOfTwo(image.Width);
+        //    int height = NextPowerOfTwo(image.Height);
+
+        //    // Create a temporary image for drawing
+        //    Bitmap tempImage = new Bitmap(width, height);
+
+        //    // Draw the original image onto the temporary image
+        //    using (Graphics g = Graphics.FromImage(tempImage))
+        //    {
+        //        g.DrawImage(image, 0, 0);
+        //    }
+
+        //    // Convert the temporary image to grayscale using AForge.NET
+        //    Grayscale grayscaleFilter = Grayscale.CommonAlgorithms.BT709;
+        //    Bitmap grayscaleImage = grayscaleFilter.Apply(tempImage);
+
+        //    // Convert the grayscale image to 8bpp indexed format
+        //    //Bitmap resultImage = Accord.Imaging.Image.Clone(grayscaleImage, PixelFormat.Format8bppIndexed);
+
+        //    return grayscaleImage;
+        //}
+
+
+        //static Bitmap PadToPowerOfTwoV2(Bitmap image, int width, int height)
+        //{
+
+        //    // Create a temporary image for drawing
+        //    Bitmap tempImage = new Bitmap(width: width, height: height);
+
+        //    // Draw the original image onto the temporary image
+        //    using (Graphics g = Graphics.FromImage(tempImage))
+        //    {
+        //        g.DrawImage(image, 0, 0);
+        //    }
+
+        //    // Convert the temporary image to grayscale using AForge.NET
+        //    Grayscale grayscaleFilter = Grayscale.CommonAlgorithms.BT709;
+        //    Bitmap grayscaleImage = grayscaleFilter.Apply(tempImage);
+
+        //    // Convert the grayscale image to 8bpp indexed format
+        //    //Bitmap resultImage = Accord.Imaging.Image.Clone(grayscaleImage, PixelFormat.Format8bppIndexed);
+
+        //    return grayscaleImage;
+        //}
+
+
+
+
+
+        //static int NextPowerOfTwo(int value)
+        //{
+        //    int power = 1;
+        //    while (power < value) power <<= 1;
+        //    return power;
+        //}
+
+        //static Bitmap PadToSize(Bitmap image, int width, int height)
+        //{
+        //    // Create a temporary image for drawing
+        //    Bitmap tempImage = new Bitmap(width, height);
+
+        //    // Draw the original image onto the temporary image
+        //    using (Graphics g = Graphics.FromImage(tempImage))
+        //    {
+        //        g.DrawImage(image, 0, 0);
+        //    }
+
+        //    // Convert the temporary image to grayscale using AForge.NET
+        //    Grayscale grayscaleFilter = Grayscale.CommonAlgorithms.BT709;
+        //    Bitmap grayscaleImage = grayscaleFilter.Apply(tempImage);
+
+        //    return grayscaleImage;
+        //}
+
+
+
+        ////static List<Point> FindPeaks(Bitmap correlationMap, double threshold = 0.9)
+        ////{
+        ////    // Convert the correlation map to a grayscale image
+        ////    Accord.Imaging.Filters.Grayscale grayscaleFilter = new Accord.Imaging.Filters.Grayscale(0.2125, 0.7154, 0.0721);
+        ////    Bitmap grayMap = grayscaleFilter.Apply(correlationMap);
+
+        ////    // Find peaks
+        ////    List<Point> peakLocations = new List<Point>();
+        ////    for (int y = 0; y < grayMap.Height; y++)
+        ////    {
+        ////        for (int x = 0; x < grayMap.Width; x++)
+        ////        {
+        ////            // Get the pixel intensity
+        ////            Color pixel = grayMap.GetPixel(x, y);
+        ////            double intensity = pixel.GetBrightness();
+
+        ////            // If intensity is above the threshold, add to peaks
+        ////            if (intensity > threshold)
+        ////            {
+        ////                peakLocations.Add(new Point(x, y));
+        ////            }
+        ////        }
+        ////    }
+
+        ////    return peakLocations;
+        ////}
+
+        //static List<Point> FindPeaks(Bitmap correlationMap, double threshold = 0.9)
+        //{
+
+        //    Bitmap grayMap = EnsureGrayscale(correlationMap);
+
+        //    // Lock the bits for fast pixel manipulation
+        //    BitmapData data = grayMap.LockBits(new Rectangle(0, 0, grayMap.Width, grayMap.Height), ImageLockMode.ReadOnly, grayMap.PixelFormat);
+
+        //    // Find peaks
+        //    List<Point> peakLocations = new List<Point>();
+
+        //    unsafe
+        //    {
+        //        byte* ptr = (byte*)data.Scan0.ToPointer();
+        //        int stride = data.Stride;
+
+        //        for (int y = 0; y < data.Height; y++)
+        //        {
+        //            for (int x = 0; x < data.Width; x++)
+        //            {
+        //                byte intensity = ptr[y * stride + x];
+
+        //                // Convert byte intensity to double (0 to 1)
+        //                double normalizedIntensity = intensity / 255.0;
+
+        //                // If intensity is above the threshold, add to peaks
+        //                if (normalizedIntensity > threshold)
+        //                {
+        //                    peakLocations.Add(new Point(x, y));
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    // Unlock the bits
+        //    grayMap.UnlockBits(data);
+
+        //    return peakLocations;
+        //}
+    }
+}
---
+++ TestImageMapping/junk2.cs
@@ -0,0 +1,236 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.Drawing.Imaging;
+//using System.Numerics;
+//using System.Threading.Tasks;
+//using Accord.Imaging;
+//using Accord.Imaging.Filters;
+
+//class Program
+//{
+
+//    static void Main()
+//    {
+//        // Load the sample image
+//        Bitmap sampleImage = new Bitmap("sample.tif");
+
+//        // Load the template image
+//        Bitmap templateImage = new Bitmap("template.tif");
+
+//        // Define the scaling factor (0 to 1, where 1 is original size)
+//        float scaleFactor = 0.3f;
+
+//        Bitmap resizedSampleImage = ResizeImage(sampleImage, (int)(sampleImage.Width * scaleFactor), (int)(sampleImage.Height * scaleFactor));
+//        Bitmap resizedTemplateImage = ResizeImage(templateImage, (int)(templateImage.Width * scaleFactor), (int)(templateImage.Height * scaleFactor));
+
+
+//        //// Define the region size and overlap
+//        //Size regionSize = new Size(templateImage.Width, templateImage.Height);
+//        //int overlap = regionSize.Height / 2;
+
+//        // Divide the sample image into overlapping regions
+//        // Define padding percentage
+//        int paddingPercentage = 20;
+
+//        // Divide the sample image into overlapping regions
+//        List<Rectangle> regions = DivideIntoOverlappingRegions(resizedSampleImage, resizedTemplateImage, paddingPercentage);
+
+
+//        // Split the image into separate Bitmaps for each region
+//        List<Bitmap> regionImages = SplitImage(resizedSampleImage, regions);
+
+//        // Perform template matching on each region
+//        List<TemplateMatch> matches = new List<TemplateMatch>();
+//        List<TemplateMatch> adjustedMatches2 = new List<TemplateMatch>();
+
+
+//        Parallel.ForEach(regionImages, (regionImage, _, index) =>
+//        {
+
+//            Console.WriteLine($"Processing image at index {index} on thread {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+//            Bitmap clonedTemplate;
+//            using (Bitmap clonedImage = new Bitmap(regionImage))
+//            {
+//                lock (resizedTemplateImage)
+//                {
+//                    clonedTemplate = new Bitmap(resizedTemplateImage);
+//                }
+//                List<TemplateMatch> regionMatches = PerformTemplateMatchingOnRegion(EnsureGrayscale(clonedImage), EnsureGrayscale(clonedTemplate));
+
+//                DrawMatchesOnImage(clonedImage, regionMatches);
+//                clonedImage.Save($"RegionImageWithMatches_{index}.png");
+
+//                Rectangle regionRect = regions[(int)index];
+
+//                // Adjust match location to map back to large image coordinates
+//                List<TemplateMatch> adjustedMatches = new List<TemplateMatch>();
+
+//                foreach (var match in regionMatches)
+//                {
+
+//                    //var adjustedRectangle = new Rectangle(match.Rectangle.X, match.Rectangle.Y + regionRect.Y, match.Rectangle.Width, match.Rectangle.Height);
+
+//                    var adjustedRectangle = new Rectangle(
+//                        match.Rectangle.X + regionRect.X, // Adjust X coordinate
+//                        match.Rectangle.Y + regionRect.Y, // Adjust Y coordinate
+//                        match.Rectangle.Width,
+//                        match.Rectangle.Height
+//                    );
+
+//                    adjustedMatches.Add(new TemplateMatch(adjustedRectangle, match.Similarity));
+//                    Console.WriteLine($"Region: {regionRect}");
+//                    Console.WriteLine($"Original Match: {match.Rectangle}");
+//                    Console.WriteLine($"Adjusted Match: {adjustedRectangle}");
+//                }
+
+//                // Add matches to the global list
+//                lock (matches)
+//                {
+//                    matches.AddRange(adjustedMatches);
+//                }
+
+//            }
+//            clonedTemplate.Dispose();
+
+//        });
+
+
+
+
+
+
+//        // Output the results
+//        Console.WriteLine("Found matches:");
+//        foreach (var match in matches)
+//        {
+//            var adjustedRectangle = new Rectangle((int)(match.Rectangle.X / scaleFactor), (int)(match.Rectangle.Y / scaleFactor), (int)(match.Rectangle.Width / scaleFactor), (int)(match.Rectangle.Height / scaleFactor));
+//            TemplateMatch AjustedMatchs = new TemplateMatch(adjustedRectangle, match.Similarity);
+//            adjustedMatches2.Add(AjustedMatchs);
+//            Console.WriteLine($"X: {AjustedMatchs.Rectangle.X}, Y: {AjustedMatchs.Rectangle.Y}, Similarity: {AjustedMatchs.Similarity}");
+//        }
+
+
+
+
+//        SaveMatchedTemplates(sampleImage, adjustedMatches2);
+//        Console.WriteLine("This is the end");
+//    }
+
+
+//    static Bitmap ResizeImage(Bitmap image, int width, int height)
+//    {
+//        Bitmap resizedImage = new Bitmap(width, height);
+//        using (Graphics graphics = Graphics.FromImage(resizedImage))
+//        {
+//            graphics.DrawImage(image, 0, 0, width, height);
+//        }
+//        return resizedImage;
+//    }
+
+
+
+//    static List<Bitmap> SplitImage(Bitmap image, List<Rectangle> regions)
+//    {
+//        List<Bitmap> regionImages = new List<Bitmap>();
+
+//        foreach (var region in regions)
+//        {
+//            Bitmap regionImage = new Bitmap(region.Width, region.Height, PixelFormat.Format24bppRgb);
+
+//            using (Graphics g = Graphics.FromImage(regionImage))
+//            {
+//                g.DrawImage(image, new Rectangle(0, 0, region.Width, region.Height), region, GraphicsUnit.Pixel);
+//            }
+
+//            regionImages.Add(regionImage);
+//        }
+
+//        return regionImages;
+//    }
+
+//    static Bitmap EnsureGrayscale(Bitmap image)
+//    {
+//        if (image.PixelFormat != PixelFormat.Format8bppIndexed)
+//        {
+//            // Convert the temporary image to grayscale using AForge.NET
+//            Grayscale grayscaleFilter = Grayscale.CommonAlgorithms.BT709;
+//            Bitmap grayscaleImage = grayscaleFilter.Apply(image);
+//            return grayscaleImage;
+//        }
+//        return image;
+//    }
+
+
+//    static List<TemplateMatch> PerformTemplateMatchingOnRegion(Bitmap regionImage, Bitmap templateImage)
+//    {
+//        ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching(0.9f); // Adjust the similarity threshold as needed
+//        TemplateMatch[] regionMatches = tm.ProcessImage(regionImage, templateImage);
+
+//        return new List<TemplateMatch>(regionMatches);
+//    }
+
+//    static List<Rectangle> DivideIntoOverlappingRegions(Bitmap image, Bitmap templateImage, int paddingPercentage)
+//    {
+//        List<Rectangle> regions = new List<Rectangle>();
+
+//        int templateWidth = templateImage.Width;
+//        int templateHeight = templateImage.Height;
+
+//        // Calculate padding based on the template size and the padding percentage
+//        int paddingWidth = (int)(templateWidth * (paddingPercentage / 100.0));
+//        int paddingHeight = (int)(templateHeight * (paddingPercentage / 100.0));
+
+//        // Ensure the region size is at least as big as the template size plus padding
+//        int regionWidth = Math.Max(image.Width, templateWidth + paddingWidth);
+//        int regionHeight = Math.Max(templateHeight + paddingHeight, templateHeight);
+
+//        // Calculate overlap
+//        int overlap = regionHeight / 6;
+
+//        for (int y = 0; y <= image.Height - regionHeight; y += regionHeight - overlap)
+//        {
+//            regions.Add(new Rectangle(0, y, regionWidth, regionHeight));
+//        }
+//        return regions;
+//    }
+
+
+
+
+
+//    static void SaveMatchedTemplates(Bitmap originalImage, List<TemplateMatch> matches)
+//    {
+//        for (int i = 0; i < matches.Count; i++)
+//        {
+//            var match = matches[i];
+//            var cropArea = match.Rectangle;
+
+//            Console.WriteLine($"Saving match {i + 1}: X: {match.Rectangle.X}, Y: {match.Rectangle.Y}, Similarity: {match.Similarity}");
+
+//            // Crop the matched template from the original image
+//            using (Bitmap matchedTemplate = originalImage.Clone(cropArea, originalImage.PixelFormat))
+//            {
+//                // Save the matched template with a unique filename
+//                string filename = $"MatchedTemplate_{match.Rectangle.X}_{match.Rectangle.Y}_{i + 1}.png";
+//                matchedTemplate.Save(filename);
+//                Console.WriteLine($"Saved {filename}");
+//            }
+//        }
+//    }
+
+//    static void DrawMatchesOnImage(Bitmap image, List<TemplateMatch> matches)
+//    {
+//        using (Graphics g = Graphics.FromImage(image))
+//        {
+//            foreach (var match in matches)
+//            {
+//                g.DrawRectangle(new Pen(Color.Red, 2), match.Rectangle);
+//            }
+//        }
+//    }
+
+
+
+
+//}
